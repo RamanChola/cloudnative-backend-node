2782fc35f9cf72e8ffc550cd81750309f6ba21f1
iota-008 <raiboleankit8@gmail.com>
2021-12-12 20:20:43 +0530

    configured .env file

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require('dotenv').config()
+require("dotenv").config();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const users = require("./routes/users");
@@ -8,25 +8,26 @@ const data = require("./routes/data");
 const app = express();
 
 mongoose
-  .connect("mongodb+srv://user:user@123@cluster0.qcxwm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
-  .then(console.log("Connected to MongoDB"))
-  .catch((err) => console.log(err));
+	.connect(process.env.MONGODB_CONNECT_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useCreateIndex: true,
+	})
+	.then(() => {
+		console.log("Connected to MongoDB");
+	})
+	.catch((err) => console.log(err));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hi There");
+	res.send("Server running without error :)");
 });
 
 app.use("/api/users", users);
 app.use("/api/data", data);
 app.listen(port, () => {
-  console.log(`app is listening at http://localhost:${port}`);
+	console.log(`app is listening at http://localhost:${port}`);
 });
-
