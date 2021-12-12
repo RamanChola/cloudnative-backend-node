6895baef1c7480b1b11b664af0f7dea4ef5f3e48
RamanChola <ramanchola3@gmail.com>
2021-12-12 22:51:08 +0530

    added flapped link to white list

--- server.js
+++ server.js
@@ -8,24 +8,28 @@ const cors = require("cors");
 const app = express();
 dotenv.config();
 mongoose
-	.connect(process.env.MONGODB_CONNECT_URL, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useCreateIndex: true,
-	})
-	.then(() => {
-		console.log("Connected to MongoDB");
-	})
-	.catch((err) => console.log(err));
+  .connect(process.env.MONGODB_CONNECT_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => console.log(err));
 
-let whitelist = ["http://localhost:3000", "https://brimo.netlify.app"];
+let whitelist = [
+  "http://localhost:3000",
+  "https://brimo.netlify.app",
+  "http://flapped.xyz",
+];
 var corsOptions = {
-	credentials: true,
-	optionsSuccessStatus: 200, // For legacy browser support
-	origin: (origin, callback) => {
-		if (whitelist.indexOf(origin) !== -1) return callback(null, true);
-		return callback(new Error("Not allowed by CORS"));
-	},
+  credentials: true,
+  optionsSuccessStatus: 200, // For legacy browser support
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) return callback(null, true);
+    return callback(new Error("Not allowed by CORS"));
+  },
 };
 
 app.use(cors(corsOptions));
@@ -33,11 +37,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-	res.send("Server running without error :)");
+  res.send("Server running without error :)");
 });
 
 app.use("/api/users", users);
 app.use("/api/data", data);
 app.listen(port, () => {
-	console.log(`app is listening at port `, port);
+  console.log(`app is listening at port `, port);
 });
